f4ed6fba3cc1b59d6acf9570000fb1237c28eee7
sungkug <tjdrnrdla@hanmail.net>
2023-05-12 00:00:52 +0900

    weather

--- dist/weather.js
+++ dist/weather.js
@@ -6,13 +6,13 @@ export class WeatherAPI {
     async fetchWeatherData(latitude, longitude) {
         console.log(latitude);
         let apikey = "HqBrGQAZCs7F7Ho61Lri4K4z%2Bk1rXJfsXL6YGpw5lQjfSgYO6cl%2FIZze%2FSu9WT80mWfaKvwEbfeKFZT5UbytKw%3D%3D";
-        let ForecastGribURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";
+        let ForecastGribURL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
         ForecastGribURL += "?ServiceKey=" + apikey;
-        ForecastGribURL += "&base_date=20230510";
-        ForecastGribURL += "&base_time=0500";
+        ForecastGribURL += "&pageNo=1&numOfRows=1000";        
+        ForecastGribURL += "&dataType=json";
+        ForecastGribURL += "&base_date=20230511";
+        ForecastGribURL += "&base_time=2300";
         ForecastGribURL += "&nx=" + latitude + "&ny=" + longitude;
-        ForecastGribURL += "&pageNo=1&numOfRows=7";
-        ForecastGribURL += "&_type=json";
         const response = await fetch(ForecastGribURL);
         const data = await response.json();
         return data;
@@ -20,8 +20,8 @@ export class WeatherAPI {
 
     getWeatherData() {
         navigator.geolocation.getCurrentPosition(async (position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
+            const latitude = Math.ceil(position.coords.latitude);
+            const longitude = Math.ceil(position.coords.longitude);            
             const weatherData = await this.fetchWeatherData(latitude, longitude);
             console.log(weatherData);
         });
